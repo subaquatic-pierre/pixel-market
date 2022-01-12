da9b05ece2c1c38e767f7c292d6d528353c5ab5f
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-01-13 03:55:40 +0400

    Installed data grid component from mui

--- frontend/src/pages/AuthorRequestListPage.tsx
+++ frontend/src/pages/AuthorRequestListPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Container from "@mui/material/Container";
+
+import AuthorRequestList from "components/AuthorRequestList";
 
 const AuthorRequestListPage = () => {
-  return <div></div>;
+  return (
+    <Container maxWidth="md">
+      <AuthorRequestList />
+    </Container>
+  );
 };
 
 export default AuthorRequestListPage;
