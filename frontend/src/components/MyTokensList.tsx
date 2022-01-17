2024c33f166358d016f4a3482871b0425d779ecc
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-01-17 08:50:50 +0400

    Start moving create, register and token listings all to my token page

--- frontend/src/components/MyTokensList.tsx
+++ frontend/src/components/MyTokensList.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import CreateIcon from "@mui/icons-material/Create";
+import Button from "@mui/material/Button";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
 
 import MyTokenListItem from "components/MyTokenListItem";
 import useDappContext from "hooks/useDappContext";
@@ -48,6 +57,32 @@ const MyTokensList = () => {
 
   return (
     <Box>
+      <Grid item>
+        <Toolbar disableGutters sx={{ mb: 1 }}>
+          <Link
+            style={{ textDecoration: "none", color: "inherit" }}
+            to="/create"
+          >
+            <Button variant="contained" sx={{ mr: 2 }}>
+              <ListItemIcon sx={{ color: "inherit", minWidth: "40px" }}>
+                <CreateIcon />
+              </ListItemIcon>
+              <ListItemText primary="New Token" />
+            </Button>
+          </Link>
+          <Link
+            style={{ textDecoration: "none", color: "inherit" }}
+            to="create-token"
+          >
+            <Button variant="contained" sx={{ mr: 2 }}>
+              <ListItemIcon sx={{ color: "inherit", minWidth: "40px" }}>
+                <HowToRegIcon />
+              </ListItemIcon>
+              <ListItemText primary="Register" />
+            </Button>
+          </Link>
+        </Toolbar>
+      </Grid>
       <Grid container spacing={4}>
         {!state.loading &&
           state.listItems.map((item, index) => (
--- frontend/src/hooks/useNavLinks.ts
+++ frontend/src/hooks/useNavLinks.ts
@@ -2,7 +2,6 @@ const navLinks = [
   { text: "Dashboard", link: "/", icon: "dashboard" },
   { text: "Wallet", link: "wallet", icon: "wallet" },
   { text: "Marketplace", link: "marketplace", icon: "market" },
-  { text: "Create Token", link: "create", icon: "create" },
   { text: "My Tokens", link: "tokens", icon: "token" },
   { text: "My Listings", link: "listings", icon: "listings" },
 ];
