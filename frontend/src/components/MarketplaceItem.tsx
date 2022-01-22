07acbaae61ee49b85cf95957061cdd6067c87910
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-01-22 18:38:41 +0400

    Started market plaec item card

--- frontend/src/components/MarketplaceItem.tsx
+++ frontend/src/components/MarketplaceItem.tsx
@@ -81,6 +81,31 @@ const MarketplaceItem: React.FC<IMarketplaceItemProps> = ({
             </Typography>
             <Typography>{description}</Typography>
           </Box>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                bullet
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
           <Box sx={{ alignSelf: "end", mt: "auto" }}>
             {!isOwner && (
               <Button variant="contained" component="label" color="secondary">
