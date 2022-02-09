b5d9e47223f2df3229fe22dd59beaa302f4c7fbb
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-02-09 22:01:01 +0400

    Middel of updating author list

--- frontend/src/components/AuthorList.tsx
+++ frontend/src/components/AuthorList.tsx
@@ -14,8 +14,8 @@ const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 150 },
   { field: "name", headerName: "Name", width: 150 },
   { field: "email", headerName: "Email", width: 150 },
-  { field: "activeStatus", headerName: "Active Status", width: 150 },
-  { field: "address", headerName: "Wallet Address", width: 370 },
+  { field: "authorStatus", headerName: "Author Status", width: 150 },
+  { field: "walletAddress", headerName: "Wallet Address", width: 370 },
 ];
 
 const AuthorRequestList = () => {
@@ -25,35 +25,50 @@ const AuthorRequestList = () => {
   const [_n, { setWarning }] = useNotificationContext();
   const navigate = useNavigate();
 
+  const parseActiveStatusCode = (code: number): string => {
+    switch (code) {
+      case 0:
+        return "None";
+      case 1:
+        return "Active";
+      case 2:
+        return "Inactive";
+      default:
+        return "NA";
+    }
+  };
+
+  const parseListingIds = (list): number[] => {
+    const listingIds = list.map((listingId) => Number(listingId.toString()));
+    return listingIds;
+  };
+
+  const parseUserRes = (userRes): IUser => {
+    const user: IUser = {
+      id: userRes.id.toString(),
+      walletAddress: userRes.walletAddress,
+      name: userRes.name,
+      email: userRes.email,
+      listingIds: [],
+      adminStatus: parseActiveStatusCode(userRes.adminStatus),
+      authorStatus: parseActiveStatusCode(userRes.authorStatus),
+    };
+
+    return user;
+  };
+
   const getAuthors = async () => {
     const marketplaceContract = dappState.contracts.pixelMarketplace;
     const userAddressList = await marketplaceContract.userAddresses();
-    // const userCount = Number(bigNumUserCount.toString());
-    const _authors = [];
-
-    console.log(userAddressList);
+    const users: IUser[] = [];
 
     for (const address of userAddressList) {
       const userRes = await marketplaceContract.users(address);
-      console.log(userRes);
-      // const authorRes = await marketplaceContract.authors(authorAddress);
-      try {
-        const _author: IUser = {
-          id: userRes.id,
-          walletAddress: userRes.walletAddress,
-          name: userRes.name,
-          email: userRes.email,
-          listingIds: userRes.listingIds,
-          adminStatus: userRes.adminStatus,
-          authorStatus: userRes.authorStatus,
-        };
-        _authors.push(_author);
-      } catch (err) {
-        setWarning(err.message);
-      }
+      const user = parseUserRes(userRes);
+      users.push(user);
     }
 
-    setAuthors(_authors);
+    setAuthors(users);
   };
 
   const handleRowClick = ({ row }: GridRowParams) => {
